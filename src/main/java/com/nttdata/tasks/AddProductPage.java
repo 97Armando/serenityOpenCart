00be03e6442660ec5d343b0e3619bef4cdac560c
John Armando Maldonado Conejo <jamaldon@bgr.com.ec>
2024-01-16 11:31:19 -0500

    Commit test funcional pruebas y reporte

--- src/main/java/com/nttdata/tasks/AddProductPage.java
+++ src/main/java/com/nttdata/tasks/AddProductPage.java
@@ -11,44 +11,33 @@ import net.serenitybdd.screenplay.waits.WaitUntil;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
-public class AddProductPage implements Task{
-    public static Task addProductsToCart(){ return instrumented(AddProductPage.class);}
+public class AddProductPage implements Task {
+    public static Task addProductsToCart() { return instrumented(AddProductPage.class); }
 
     @Override
-    public <T extends Actor>void perfomAs(T actor){
+    public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
-                //BUSCA LOS ELEMENTOS Y DA CLICK EN LOS IDENTIFICADOS PRODUCTO 1
-                WaitUntil.the(ShopPageInterface.BTN_ADD_TO_CART_1,isVisible()).forNoMoreThan(20).seconds(),
+                WaitUntil.the(ShopPageInterface.BTN_ADD_TO_CART_1, isVisible()).forNoMoreThan(20).seconds(),
                 Click.on(ShopPageInterface.BTN_ADD_TO_CART_1),
 
-                //BUSCA LOS ELEMENTOS Y DA CLICK EN LOS IDENTIFICADOS PRODUCTO 2
-                WaitUntil.the(ShopPageInterface.BTN_ADD_TO_CART_2,isVisible()).forNoMoreThan(20).seconds(),
+                WaitUntil.the(ShopPageInterface.BTN_ADD_TO_CART_2, isVisible()).forNoMoreThan(10).seconds(),
                 Click.on(ShopPageInterface.BTN_ADD_TO_CART_2),
 
-                //BUSCA LOS ELEMENTOS Y DA CLICK EN ICONO CARRITO
-                WaitUntil.the(ShopPageInterface.BTN_CART,isVisible()).forNoMoreThan(20).seconds(),
+                WaitUntil.the(ShopPageInterface.BTN_CART, isVisible()).forNoMoreThan(20).seconds(),
                 Click.on(ShopPageInterface.BTN_CART),
 
-                //BUSCA LOS ELEMENTOS Y DA CLICK EN VER CARRITO
-                WaitUntil.the(ShopPageInterface.BTN_VIEW_CART,isVisible()).forNoMoreThan(20).seconds(),
+                WaitUntil.the(ShopPageInterface.BTN_VIEW_CART, isVisible()).forNoMoreThan(20).seconds(),
                 Click.on(ShopPageInterface.BTN_VIEW_CART),
 
-                //BUSCA LOS ELEMENTOS Y DA CLICK IR A PAGAR
-                WaitUntil.the(ShopPageInterface.BTN_CHECKOUT,isVisible()).forNoMoreThan(20).seconds(),
+                WaitUntil.the(ShopPageInterface.BTN_CHECKOUT, isVisible()).forNoMoreThan(20).seconds(),
+                Scroll.to(ShopPageInterface.BTN_CHECKOUT),
                 Click.on(ShopPageInterface.BTN_CHECKOUT),
 
-                //BUSCA LOS ELEMENTOS Y DA CLICK EN COMPRAR COMO INVITADO
-                WaitUntil.the(ShopPageInterface.BTN_GUEST_CHECKOUT,isVisible()).forNoMoreThan(20).seconds(),
+                WaitUntil.the(ShopPageInterface.BTN_GUEST_CHECKOUT, isVisible()).forNoMoreThan(20).seconds(),
                 Click.on(ShopPageInterface.BTN_GUEST_CHECKOUT),
 
-                //BUSCA LOS ELEMENTOS Y DA CLICK EN COMPRAR COMO INVITADO
-                WaitUntil.the(ShopPageInterface.BTN_CONTINUE,isVisible()).forNoMoreThan(20).seconds(),
+                WaitUntil.the(ShopPageInterface.BTN_CONTINUE, isVisible()).forNoMoreThan(20).seconds(),
                 Click.on(ShopPageInterface.BTN_CONTINUE)
-
-
-
         );
-
     }
 }
-
--- src/main/java/com/nttdata/tasks/PurchaseFormPage.java
+++ src/main/java/com/nttdata/tasks/PurchaseFormPage.java
@@ -1,5 +1,4 @@
 package com.nttdata.tasks;
-
 import com.nttdata.userinterfaces.PurchasePageInterface;
 import com.nttdata.userinterfaces.ShopPageInterface;
 import net.serenitybdd.screenplay.Actor;
@@ -37,68 +36,69 @@ public class PurchaseFormPage implements Task {
         this.postcode = postcode;
         this.country = country;
         this.region = region;
-
-
-        public static Performable withData (String firstname, String lastname, String email, String telephone, String company, String address1, String city, String postcode, String countryId, String regionId){
-            return instrumented(PurchaseFormPage.class, firstname, lastname, email, telephone, company, address1, city, postcode, countryId, regionId);
-        }
-        @Override
-        public <T extends Actor> void perfomAs (T actor){
-            actor.attemptsTo(
-                    WaitUntil.the(PurchasePageInterface.INP_FIRST_NAME, isVisible()).forNoMoreThan(20).seconds(),
-                    Enter.theValue(firstname).into(PurchasePageInterface.INP_FIRST_NAME),
-
-                    WaitUntil.the(PurchasePageInterface.INP_LAST_NAME, isVisible()).forNoMoreThan(20).seconds(),
-                    Enter.theValue(firstname).into(PurchasePageInterface.INP_LAST_NAME),
-
-                    WaitUntil.the(PurchasePageInterface.INP_EMAIL, isVisible()).forNoMoreThan(20).seconds(),
-                    Enter.theValue(firstname).into(PurchasePageInterface.INP_EMAIL),
-
-                    WaitUntil.the(PurchasePageInterface.INP_PHONE, isVisible()).forNoMoreThan(20).seconds(),
-                    Enter.theValue(firstname).into(PurchasePageInterface.INP_PHONE),
-
-                    WaitUntil.the(PurchasePageInterface.INP_COMPANY, isVisible()).forNoMoreThan(20).seconds(),
-                    Enter.theValue(firstname).into(PurchasePageInterface.INP_COMPANY),
-
-                    WaitUntil.the(PurchasePageInterface.INP_ADDRESS, isVisible()).forNoMoreThan(20).seconds(),
-                    Enter.theValue(firstname).into(PurchasePageInterface.INP_ADDRESS),
-
-                    WaitUntil.the(PurchasePageInterface.INP_CITY, isVisible()).forNoMoreThan(20).seconds(),
-                    Enter.theValue(firstname).into(PurchasePageInterface.INP_CITY),
-
-                    WaitUntil.the(PurchasePageInterface.INP_POST_CODE, isVisible()).forNoMoreThan(20).seconds(),
-                    Enter.theValue(firstname).into(PurchasePageInterface.INP_POST_CODE),
-
-                    Scroll.to(PurchasePageInterface.OPT_REGION),
-
-                    WaitUntil.the(PurchasePageInterface.INP_COUNTRY, isVisible()).forNoMoreThan(20).seconds(),
-                    Enter.theValue(firstname).into(PurchasePageInterface.INP_COUNTRY),
-
-                    WaitUntil.the(PurchasePageInterface.INP_REGION, isVisible()).forNoMoreThan(20).seconds(),
-                    Enter.theValue(firstname).into(PurchasePageInterface.INP_REGION),
-
-                    WaitUntil.the(PurchasePageInterface.OPT_REGION, isVisible()).forNoMoreThan(20).seconds(),
-                    Enter.theValue(firstname).into(PurchasePageInterface.OPT_REGION),
-
-                    WaitUntil.the(PurchasePageInterface.BTN_CONTINUE, isVisible()).forNoMoreThan(20).seconds(),
-                    Enter.theValue(firstname).into(PurchasePageInterface.BTN_CONTINUE),
-
-                    WaitUntil.the(PurchasePageInterface.BTN_SHIPPING, isVisible()).forNoMoreThan(20).seconds(),
-                    Enter.theValue(firstname).into(PurchasePageInterface.BTN_SHIPPING),
-
-                    WaitUntil.the(PurchasePageInterface.BTN_TERMS, isVisible()).forNoMoreThan(20).seconds(),
-                    Enter.theValue(firstname).into(PurchasePageInterface.BTN_TERMS),
-
-                    WaitUntil.the(PurchasePageInterface.BTN_CONTINUE2, isVisible()).forNoMoreThan(20).seconds(),
-                    Enter.theValue(firstname).into(PurchasePageInterface.BTN_CONTINUE2),
-
-                    WaitUntil.the(PurchasePageInterface.BTN_CONF, isVisible()).forNoMoreThan(20).seconds(),
-                    Enter.theValue(firstname).into(PurchasePageInterface.BTN_CONF),
-
-                    WaitUntil.the(PurchasePageInterface.btn_final, isVisible()).forNoMoreThan(20).seconds(),
-                    Click.on(PurchasePageInterface.btn_final)
-
-            );
-        }
     }
+
+    public static Performable withData(String firstname, String lastname, String email, String telephone, String company, String address1, String city, String postcode, String countryId, String regionId) {
+        return instrumented(PurchaseFormPage.class, firstname, lastname, email, telephone, company, address1, city, postcode, countryId, regionId);
+    }
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                WaitUntil.the(PurchasePageInterface.INP_FIRST_NAME, isVisible()).forNoMoreThan(20).seconds(),
+                Enter.theValue(firstname).into(PurchasePageInterface.INP_FIRST_NAME),
+
+                WaitUntil.the(PurchasePageInterface.INP_LAST_NAME, isVisible()).forNoMoreThan(20).seconds(),
+                Enter.theValue(lastname).into(PurchasePageInterface.INP_LAST_NAME),
+
+                WaitUntil.the(PurchasePageInterface.INP_EMAIL, isVisible()).forNoMoreThan(20).seconds(),
+                Enter.theValue(email).into(PurchasePageInterface.INP_EMAIL),
+
+                WaitUntil.the(PurchasePageInterface.INP_PHONE, isVisible()).forNoMoreThan(20).seconds(),
+                Enter.theValue(telephone).into(PurchasePageInterface.INP_PHONE),
+
+                WaitUntil.the(PurchasePageInterface.INP_COMPANY, isVisible()).forNoMoreThan(20).seconds(),
+                Enter.theValue(company).into(PurchasePageInterface.INP_COMPANY),
+
+                WaitUntil.the(PurchasePageInterface.INP_ADDRESS, isVisible()).forNoMoreThan(20).seconds(),
+                Enter.theValue(address_1).into(PurchasePageInterface.INP_ADDRESS),
+
+                WaitUntil.the(PurchasePageInterface.INP_CITY, isVisible()).forNoMoreThan(20).seconds(),
+                Enter.theValue(city).into(PurchasePageInterface.INP_CITY),
+
+                WaitUntil.the(PurchasePageInterface.INP_POST_CODE, isVisible()).forNoMoreThan(20).seconds(),
+                Enter.theValue(postcode).into(PurchasePageInterface.INP_POST_CODE),
+
+                Scroll.to(PurchasePageInterface.OPT_REGION),
+
+                WaitUntil.the(PurchasePageInterface.INP_COUNTRY, isVisible()).forNoMoreThan(20).seconds(),
+                Click.on(PurchasePageInterface.INP_COUNTRY),
+
+                WaitUntil.the(PurchasePageInterface.INP_REGION, isVisible()).forNoMoreThan(20).seconds(),
+                Click.on(PurchasePageInterface.INP_REGION),
+
+                WaitUntil.the(PurchasePageInterface.OPT_REGION, isVisible()).forNoMoreThan(20).seconds(),
+                Click.on(PurchasePageInterface.OPT_REGION),
+
+                WaitUntil.the(PurchasePageInterface.BTN_CONTINUE, isVisible()).forNoMoreThan(20).seconds(),
+                Click.on(PurchasePageInterface.BTN_CONTINUE),
+
+                WaitUntil.the(PurchasePageInterface.BTN_SHIPPING, isVisible()).forNoMoreThan(20).seconds(),
+                Click.on(PurchasePageInterface.BTN_SHIPPING),
+
+                WaitUntil.the(PurchasePageInterface.BTN_TERMS, isVisible()).forNoMoreThan(20).seconds(),
+                Click.on(PurchasePageInterface.BTN_TERMS),
+
+                WaitUntil.the(PurchasePageInterface.BTN_CONTINUE2, isVisible()).forNoMoreThan(20).seconds(),
+                Click.on(PurchasePageInterface.BTN_CONTINUE2),
+
+                WaitUntil.the(PurchasePageInterface.BTN_CONF, isVisible()).forNoMoreThan(20).seconds(),
+                Click.on(PurchasePageInterface.BTN_CONF),
+
+                WaitUntil.the(PurchasePageInterface.btn_final, isVisible()).forNoMoreThan(20).seconds(),
+                Click.on(PurchasePageInterface.btn_final)
+
+        );
+    }
+
 }
